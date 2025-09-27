7a4d461a14c4d28a2b3296c536f51e3c55dc35c1
Aditya Varudkar <adityavarudkar95@gmail.com>
2025-09-27 19:43:43 +0530

    Create 1A.cpp

---
+++ 1A.cpp
@@ -0,0 +1,18 @@
+// Name: Aditya Varudkar 
+// PRN: 25070123506
+// Experiment 1(A)
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+/* Output
+
+Hello, World!
+
+*/
